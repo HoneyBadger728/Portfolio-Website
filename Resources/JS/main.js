1da57a692cf444b9f43ff964b90dca86c9e2d319
bDavis <BlakelyJDavis@gmail.com>
2024-11-24 08:57:41 -0500

    created contacts and tweaked transitions

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -3,6 +3,7 @@ function showSplash() {
     document.querySelector('.splash-content').style.opacity = '1';
     document.querySelector('.splash-content').style.visibility = 'visible';
     document.querySelector('.about').classList.add('hidden');
+    document.querySelector('.contact').classList.add('hidden');
 }
 
 // Show the About section and hide the Splash content
@@ -10,4 +11,14 @@ function showAbout() {
     document.querySelector('.splash-content').style.opacity = '0';
     document.querySelector('.splash-content').style.visibility = 'hidden';
     document.querySelector('.about').classList.remove('hidden');
+    document.querySelector('.contact').classList.add('hidden');
+
+}
+
+// Show the Contact section and hide the About section
+function showContact() {
+    document.querySelector('.splash-content').style.opacity = '0';
+    document.querySelector('.splash-content').style.visibility = 'hidden';
+    document.querySelector('.about').classList.add('hidden');
+    document.querySelector('.contact').classList.remove('hidden');
 }
