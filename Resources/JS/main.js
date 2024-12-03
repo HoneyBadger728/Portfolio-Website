3564d26feb66dd36b5e0785fc4189830ae0f5f24
bDavis <BlakelyJDavis@gmail.com>
2024-12-03 02:44:29 -0500

    textTypingEffect( ) working and CSS modified to suit the changes

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -25,7 +25,7 @@ const navbarToggle = document.querySelector('.mobile-navbar-toggle');
     
     // Show the About section and hide the Splash content
     function showAbout() {
-        document.querySelector('.splash-content').classList.add('hidden')
+        document.querySelector('.splash-content').classList.add('hidden');
         document.querySelector('.about').classList.remove('hidden');
         document.querySelector('.contact').classList.add('hidden');
     
@@ -33,10 +33,35 @@ const navbarToggle = document.querySelector('.mobile-navbar-toggle');
     
     // Show the Contact section and hide the About section
     function showContact() {
-        document.querySelector('.splash-content').classList.add('hidden')
+        document.querySelector('.splash-content').classList.add('hidden');
         document.querySelector('.about').classList.add('hidden');
         document.querySelector('.contact').classList.remove('hidden');
     }
     
-    
+const splashH1 = document.querySelector(".splash-content h1");
+const splashH2 = document.querySelector(".splash-content h2")
+const text1 = splashH1.textContent; 
+const text2 = splashH2.textContent;
 
+    function textTypingEffect(element, text, speed, i = 0, callback = null) {
+        if (i === 0) {
+            element.textContent = "";
+        }
+        
+        element.textContent += text[i]
+
+        if (i === text.length - 1) {
+            if (callback) callback();
+            return;
+        }
+
+        setTimeout(() => textTypingEffect(element, text, speed, i + 1, callback), speed);
+
+    }
+
+    textTypingEffect(splashH1, text1, 100, 0, () => {
+        splashH2.classList.add("visible");
+        textTypingEffect(splashH2, text2, 40);
+             
+       
+    });
