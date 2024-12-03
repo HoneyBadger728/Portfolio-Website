8cd6f12960efbd4e3b351a10db692d13795c2b44
bDavis <BlakelyJDavis@gmail.com>
2024-12-02 22:37:13 -0500

    added hidden to splash content in main.js & added .hidden in css

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -18,28 +18,25 @@ const navbarToggle = document.querySelector('.mobile-navbar-toggle');
     // Show the Splash content and hide the About section
 
     function showSplash() {
-    document.querySelector('.splash-content').style.opacity = '1';
-    document.querySelector('.splash-content').style.visibility = 'visible';
-    document.querySelector('.about').classList.add('hidden');
-    document.querySelector('.contact').classList.add('hidden');
-}
-
-// Show the About section and hide the Splash content
-function showAbout() {
-    document.querySelector('.splash-content').style.opacity = '0';
-    document.querySelector('.splash-content').style.visibility = 'hidden';
-    document.querySelector('.about').classList.remove('hidden');
-    document.querySelector('.contact').classList.add('hidden');
-
-}
-
-// Show the Contact section and hide the About section
-function showContact() {
-    document.querySelector('.splash-content').style.opacity = '0';
-    document.querySelector('.splash-content').style.visibility = 'hidden';
-    document.querySelector('.about').classList.add('hidden');
-    document.querySelector('.contact').classList.remove('hidden');
-}
-
-
+        document.querySelector('.splash-content').classList.remove('hidden');
+        document.querySelector('.about').classList.add('hidden');
+        document.querySelector('.contact').classList.add('hidden');
+    }
+    
+    // Show the About section and hide the Splash content
+    function showAbout() {
+        document.querySelector('.splash-content').classList.add('hidden')
+        document.querySelector('.about').classList.remove('hidden');
+        document.querySelector('.contact').classList.add('hidden');
+    
+    }
+    
+    // Show the Contact section and hide the About section
+    function showContact() {
+        document.querySelector('.splash-content').classList.add('hidden')
+        document.querySelector('.about').classList.add('hidden');
+        document.querySelector('.contact').classList.remove('hidden');
+    }
+    
+    
 
